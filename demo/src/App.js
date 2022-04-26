3942a604596968096114d12e806c38506b3b3210
Jay Zeng <jayzeng@users.noreply.github.com>
2022-04-26 00:43:22 -0700

    Update App.js

--- demo/src/App.js
+++ demo/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          DEMO app
+          Hello world
         </p>
         <a
           className="App-link"
